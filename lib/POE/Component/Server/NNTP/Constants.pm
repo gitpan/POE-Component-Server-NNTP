1a0bd032b4bbdc35f04c94ab19fd854350567782
Chris Williams <chris@bingosnet.co.uk>
2006-12-01 14:00:35 +0000

    Import of BINGOS/POE-Component-Server-NNTP-0.95 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-NNTP
    gitpan-cpan-version:      0.95
    gitpan-cpan-path:         BINGOS/POE-Component-Server-NNTP-0.95.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/POE/Component/Server/NNTP/Constants.pm
@@ -0,0 +1,62 @@
+package POE::Component::Server::NNTP::Constants;
+
+use strict;
+use vars qw($VERSION);
+
+$VERSION = '0.01';
+
+require Exporter;
+our @ISA = qw( Exporter );
+our %EXPORT_TAGS = ( 'ALL' => [ qw( NNTPD_EAT_NONE NNTPD_EAT_CLIENT NNTPD_EAT_PLUGIN NNTPD_EAT_ALL ) ] );
+Exporter::export_ok_tags( 'ALL' );
+
+# Our constants
+sub NNTPD_EAT_NONE	() { 1 }
+sub NNTPD_EAT_CLIENT	() { 2 }
+sub NNTPD_EAT_PLUGIN	() { 3 }
+sub NNTPD_EAT_ALL	() { 4 }
+
+1;
+__END__
+
+=head1 NAME
+
+POE::Component::Server::NNTP::Constants - importable constants for POE::Component::Server::NNTP plugins.
+
+=head1 SYNOPSIS
+
+  use POE::Component::Server::NNTP::Constants qw(:ALL);
+
+=head1 DESCRIPTION
+
+POE::Component::Server::NNTP::Constants defines a number of constants that are required by the plugin system.
+
+=head1 EXPORTS
+
+=over
+
+=item NNTPD_EAT_NONE
+
+Value: 1
+
+=item NNTPD_EAT_CLIENT
+
+Value: 2
+
+=item NNTPD_EAT_PLUGIN
+
+Value: 3
+
+=item NNTPD_EAT_ALL
+
+Value: 4
+
+=back
+
+=head1 MAINTAINER
+
+Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
+
+=head1 SEE ALSO
+
+L<POE::Component::Server::NNTP>
